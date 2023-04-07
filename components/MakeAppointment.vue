071183b919ff1721b170f79782846f7b0bf6483e
lowski11 <lowsk1ll@protonmail.com>
2023-04-08 02:21:13 +0300

    add make appointment

--- components/MakeAppointment.vue
+++ components/MakeAppointment.vue
@@ -7,16 +7,17 @@
             </div>
 
             <div class="form-inputs">
-                <input type="text" class="form-input" placeholder="Имя">
-                <input type="text" class="form-input" placeholder="Фамилия">
-                <input type="text" class="form-input" placeholder="Номер телефона">
-                <input type="text" class="form-input" placeholder="Email">
+                <input type="text" class="form-input" id="form_first_name" placeholder="Имя">
+                <input type="text" class="form-input" id="form_last_name" placeholder="Фамилия">
+                <input type="text" class="form-input" id="form_phone" placeholder="Номер телефона">
+                <input type="text" class="form-input" id="form_email" placeholder="Email">
+                <input type="text" class="form-input" style="display: none" id="form_recording_date" :value="date + ' ' + hour" placeholder="Дата записи">
             </div>
 
             <p class="date-info">Дата записи: {{ date }} в {{ hour }}</p>
 
             <div class="btn-row">
-                <input class="form-btn" type="submit" value="Записаться">
+                <input class="form-btn" type="submit" value="Записаться" @click="sendPostMakeAppointment()">
             </div>
         </div>
     </div>
@@ -25,12 +26,29 @@
 <script>
 export default {
     setup() {
-        
+
     },
     methods: {
         hideMakeAppointment() {
             document.getElementById('make-appointment').style.display = 'none';
         },
+        sendPostMakeAppointment() {
+            const first_name = document.getElementById("form_first_name").value;
+            const last_name = document.getElementById("form_last_name").value;
+            const phone = document.getElementById("form_phone").value;
+            const email = document.getElementById("form_email").value;
+            const recording_date = document.getElementById("form_recording_date").value;
+
+            fetch('http://localhost:3000/api/make_appointment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ first_name, last_name, phone, email, recording_date })
+            }).then(res => res.json()) // response type
+                .then(response => console.log('Success:', JSON.stringify(response))) // log the response to console for debugging purposes
+                .catch(error => console.error('Error:', error)); // log the error to console for debugging purposes
+        }
     },
     props: {
         date: {
--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,14 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const db = new sqlite3.Database('calendar.db');
 
+
+const transporter = nodemailer.createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/bin/mail'
+});
+
+
 // Создание таблицы в базе данных
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS records (id INTEGER, first_name TEXT, last_name TEXT, phone TEXT, email TEXT, recording_date TEXT, date_added TEXT)");
@@ -22,13 +30,32 @@ app.get('/records', async (req, res) => {
     });
 });
 
-app.post('/add_record', async (req, res) => {
+app.post('/make_appointment', async (req, res) => {
     const { first_name, last_name, phone, email, recording_date } = req.body;
     const current_date = new Date().toISOString();
+
+    console.log(first_name, last_name, phone, email, recording_date)
 
     db.run(`INSERT INTO records (first_name, last_name, phone, email, recording_date, date_added) VALUES ('${first_name}', '${last_name}', '${phone}', '${email}', '${recording_date}', '${current_date}')`, function(err) {
         if (err) {
             return console.log(err.message);
+        }
+    });
+
+    const mailOptions = {
+        from: 'robot',
+        to: 'fds.fdsfdsfsd@bk.ru',
+        subject: 'Appointment',
+        text: 'Testing email from Nuxt'
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('sent');
         }
     });
 
